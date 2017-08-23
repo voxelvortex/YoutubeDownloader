5b960654ea2435c932d67eefeef851d86d68fbb7
Michael <maestromike5@gmail.com>
2017-08-23 10:39:50 -0400

    Pushing current progress...

--- ProcessStarter.py
+++ ProcessStarter.py
@@ -4,61 +4,28 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5 import QtTest
 from PyQt5 import *
-from PyQt5 import QtWidgets, QtCore, QtWebKitWidgets,QtGui
+from PyQt5 import QtWidgets, QtCore, QtWebKitWidgets,QtGui,QtWebKit,QtWebSockets,QtWebChannel
 from PyQt5.QtWebKit import QWebSettings
 from PyQt5.QtWebKitWidgets import QWebPage
 import os
 import subprocess
 import youtube_dl
+import MainWindow
+from MainWindow import *
 
-class App(QWidget):
-
+class App(QWidget,Ui_MainWindow):
     def __init__(self):
-        super().__init__()
-        self.title = 'Youtube Downloader'
-        self.initUI()
+        super(App, self).__init__()
 
-    def initUI(self):
-        self.setObjectName("MainWindow")
-        self.resize(824, 696)
-        self.setMinimumSize(QtCore.QSize(250, 150))
-        self.centralwidget = QtWidgets.QWidget(self)
-        self.centralwidget.setEnabled(True)
-        self.centralwidget.setMaximumSize(QtCore.QSize(1218, 836))
-        self.centralwidget.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.centralwidget.setObjectName("centralwidget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.urlInput = QtWidgets.QLineEdit(self.centralwidget)
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        self.urlInput.setFont(font)
-        self.urlInput.setClearButtonEnabled(False)
-        self.urlInput.setObjectName("urlInput")
-        self.verticalLayout.addWidget(self.urlInput)
-        self.setURL = QtWidgets.QPushButton(self.centralwidget)
-        self.setURL.setObjectName("setURL")
-        self.verticalLayout.addWidget(self.setURL)
-        self.webView = QtWebKitWidgets.QWebView(self.centralwidget)
-        self.webView.setUrl(QtCore.QUrl("https://www.youtube.com/"))
-        self.webView.setObjectName("webView")
-        self.verticalLayout.addWidget(self.webView)
-        self.downloadButton = QtWidgets.QPushButton(self.centralwidget)
-        self.downloadButton.setObjectName("downloadButton")
-        self.verticalLayout.addWidget(self.downloadButton)
-
-
-        _translate = QtCore.QCoreApplication.translate
-        self.setWindowTitle(_translate("MainWindow", "Youtube Downloader"))
-        self.urlInput.setText(_translate("MainWindow", "Enter URL here (include http://www)"))
-        self.setURL.setText(_translate("MainWindow", "Go to entered URL"))
-        self.downloadButton.setText(_translate("MainWindow", "Download Youtube Video"))
+        self.ui = Ui_MainWindow
+        self.ui.setupUi(self, self)
+        QtWebKit.QWebSettings.globalSettings().setAttribute(QtWebKit.QWebSettings.PluginsEnabled,True)
+        self.show()
 
         self.downloadButton.clicked.connect(self.youtubeDownload)
         self.setURL.clicked.connect(self.setURLm)
         self.getActualURL()
         self.webView.installEventFilter(self)
-        self.show()
 
     def eventFilter(self, obj, event):
         if obj == self.webView:
